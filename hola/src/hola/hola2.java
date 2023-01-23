b8368c218e95deb08751e85f13444bc1ea06e3bb
ubuntu <ubuntu@ubuntu-VirtualBox>
2023-01-23 14:19:29 +0100

    actu 2

--- hola/src/hola/hola2.java
+++ hola/src/hola/hola2.java
@@ -5,6 +5,7 @@ public class hola2 {
 	public static void main(String[] args) {
 		System.out.println("holaaaaaa");
 		System.out.println("jashfjdhgadsfhkaskdljfklajsdfhasjkldfna");
+		System.out.println("ahdsalbjav");
 	}
 
 }
